bca791ebb3acf2aa3e32243f03cdd921d8b1dbe6
dell <dell@DESKTOP-EJDMHJB>
2022-07-11 18:25:53 +0530

    first commit

---
+++ ControlLoop/src/ControlLoop/Loop3.java
@@ -0,0 +1,20 @@
+package ControlLoop;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String lang="java";
+		if(lang=="python") {
+			System.out.println("language is python");
+		}else if(lang=="c++") {
+			System.out.println("language is c++");
+		}else if(lang=="java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println(lang);
+		}
+	}
+
+}
