787f7a981f887812e5435b8d76068497b3103ff9
dell <dell@DESKTOP-EJDMHJB>
2022-07-14 20:24:34 +0530

    first commit

---
+++ Package1/src/package1/C.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class C {
+	public void msg() {
+		System.out.println("Hello from class C");
+}
+}
---
+++ Package1/src/package1/Class_B.java
@@ -0,0 +1,14 @@
+package package1;
+import package1.A;
+import package1.C;
+public class Class_B {
+
+	public static void main(String[] args) {
+		A obj=new A();
+		C obj1=new C();
+		obj.msg();
+		obj1.msg();
+
+	}
+
+}
