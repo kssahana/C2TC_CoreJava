c608576b1e14576bb1db58650165b5a5af2c4668
dell <dell@DESKTOP-EJDMHJB>
2022-07-14 20:25:08 +0530

    first commit

---
+++ This/src/this1/Main.java
@@ -0,0 +1,21 @@
+package this1;
+
+class Hello{
+	Hello()
+	{
+		this(5);
+		System.out.println("hello a");
+	}
+	Hello(int x)
+	{
+		System.out.println(x);
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello a=new Hello();
+	}
+
+}
---
+++ This/src/this1/TestThis5.java
@@ -0,0 +1,21 @@
+package this1;
+
+class Demo{
+	Demo()
+	{
+		System.out.println("hello a");
+	}
+	Demo(int x)
+	{
+		this();
+		System.out.println(x);
+	}
+}
+public class TestThis5 {
+
+	public static void main(String[] args) {
+		Demo a=new Demo(10);
+
+	}
+
+}
