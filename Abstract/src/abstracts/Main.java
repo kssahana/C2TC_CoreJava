2b8daf32e01acda20355b8e3907cb00077a3af95
dell <dell@DESKTOP-EJDMHJB>
2022-07-26 19:19:25 +0530

    first commit

---
+++ Abstract/src/abstracts/Main.java
@@ -0,0 +1,28 @@
+package abstracts;
+
+abstract class Movie{
+	abstract String setTitle(String name);
+
+	
+	
+}
+class mymovie extends Movie{
+	String setTitle(String name) {
+		return name;
+		
+	}
+
+	String setTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		Movie m=new mymovie();
+		System.out.println(m.setTitle("ranna"));
+
+	}
+
+}
